5acc64c4e68fbb4f2c74dfa09fb6c4a90f6d32e1
Reda-23 <readhajjaj@gmail.com>
2022-11-06 12:13:30 +0100

    Setting the Security to UserDetail Authentication

--- src/main/java/org/securityapp/security/AppSecurity.java
+++ src/main/java/org/securityapp/security/AppSecurity.java
@@ -2,27 +2,56 @@ package org.securityapp.security;
 
 
 
+import org.securityapp.entities.AppUser;
+import org.securityapp.services.AccountService;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class AppSecurity extends WebSecurityConfigurerAdapter {
+    private AccountService accountService;
+
+    public AppSecurity(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable();
+    // http.csrf().disable();
     http.headers().frameOptions().disable();
-    http.authorizeRequests().anyRequest().permitAll();
+    http.formLogin();
+    http.authorizeRequests().anyRequest().authenticated();
 
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(new UserDetailsService() {
+                @Override
+                public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                  AppUser appUser =  accountService.loadUserByUserName(username);
+                    Collection<GrantedAuthority> authorities = new ArrayList<>();
+                    appUser.getAppRoles().forEach(au -> {
+                        authorities.add(new SimpleGrantedAuthority(au.getRolename()));
+                    });
+                    return new User(appUser.getUsername(),appUser.getPassword(),authorities);
 
+                }
+            });
     }
 
 
