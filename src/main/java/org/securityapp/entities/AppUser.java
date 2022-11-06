9392b9c33f85717cb7a880ce51a4f9c8f5515612
Reda-23 <readhajjaj@gmail.com>
2022-11-06 11:58:19 +0100

    hidding the password

--- src/main/java/org/securityapp/entities/AppUser.java
+++ src/main/java/org/securityapp/entities/AppUser.java
@@ -1,5 +1,6 @@
 package org.securityapp.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +17,7 @@ public class AppUser {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String password;
     @ManyToMany (fetch = FetchType.EAGER)
     private Collection<AppRole> appRoles = new ArrayList<>();
