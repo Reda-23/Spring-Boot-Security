206b8258a7e0a8ea828de19ee54ff0f386ee0475
Reda-23 <readhajjaj@gmail.com>
2022-11-06 10:49:22 +0100

    Creating the Security Class

---
+++ src/main/java/org/securityapp/security/AppSecurity.java
@@ -0,0 +1,11 @@
+package org.securityapp.security;
+
+
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+public class AppSecurity extends WebSecurityConfigurerAdapter {
+
+
+}
