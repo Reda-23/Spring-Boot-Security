89d161714d5c2af4d2b98ef56b38f2351d77451a
Reda-23 <readhajjaj@gmail.com>
2022-11-06 09:13:10 +0100

    creating the user entity

---
+++ src/main/java/org/securityapp/entities/AppUser.java
@@ -0,0 +1,21 @@
+package org.securityapp.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class AppUser {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long username;
+    private Long password;
+    @ManyToMany
+    private Collection<AppRole> appRoles;
+}
