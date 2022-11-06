c164a766186ddf623ad0c0b5df7ef775603f7d3c
Reda-23 <readhajjaj@gmail.com>
2022-11-06 11:38:10 +0100

    Adding the Post Methodes To the rest constroller

--- src/main/java/org/securityapp/web/AccountRestController.java
+++ src/main/java/org/securityapp/web/AccountRestController.java
@@ -1,11 +1,11 @@
 package org.securityapp.web;
 
 
+import lombok.Data;
+import org.securityapp.entities.AppRole;
 import org.securityapp.entities.AppUser;
 import org.securityapp.services.AccountServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +23,24 @@ public class AccountRestController {
     List<AppUser> appUserList(){
         return accountService.listusers();
     }
+
+    @PostMapping(path = "/users")
+    AppUser saveUser(@RequestBody AppUser appUser){
+        return  accountService.addNewUser(appUser);
+    }
+    @PostMapping(path = "/roles")
+    AppRole saveRole(AppRole appRole){
+        return accountService.addNewRole(appRole);
+    }
+
+    @PostMapping(path = "/addRoleToUser")
+    public void addRoleToUser(@RequestBody RoleUserForm roleUserForm){
+        accountService.addRoleToUser(roleUserForm.getUserName(), roleUserForm.getRoleName());
+    }
+}
+
+@Data
+class RoleUserForm{
+    private String userName;
+    private String roleName;
 }
