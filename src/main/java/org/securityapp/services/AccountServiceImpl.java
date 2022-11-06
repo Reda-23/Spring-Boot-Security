055a3e1c1c9891a1ed83c8693d3deddbf2adc639
Reda-23 <readhajjaj@gmail.com>
2022-11-06 11:21:37 +0100

    hashing the password using BCrypt

--- src/main/java/org/securityapp/services/AccountServiceImpl.java
+++ src/main/java/org/securityapp/services/AccountServiceImpl.java
@@ -4,6 +4,7 @@ import org.securityapp.entities.AppRole;
 import org.securityapp.entities.AppUser;
 import org.securityapp.repo.AppRoleRepository;
 import org.securityapp.repo.AppUserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,14 +16,18 @@ public class AccountServiceImpl implements AccountService {
 
     private final AppRoleRepository appRoleRepository;
     private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public AccountServiceImpl(AppRoleRepository appRoleRepository, AppUserRepository appUserRepository) {
+    public AccountServiceImpl(AppRoleRepository appRoleRepository, AppUserRepository appUserRepository, PasswordEncoder passwordEncoder) {
         this.appRoleRepository = appRoleRepository;
         this.appUserRepository = appUserRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public AppUser addNewUser(AppUser appUser) {
+        String pw = appUser.getPassword();
+        appUser.setPassword(passwordEncoder.encode(pw));
         return appUserRepository.save(appUser);
     }
 
